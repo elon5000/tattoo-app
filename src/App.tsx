db83895d6980499b56d5b3fb1a353b3658cb819b
Elon Barzani <elon5000@gmail.com>
2023-05-04 02:08:42 +0300

    unneceserry code removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return <div className="app">
-    
+
   </div>
 }
 
